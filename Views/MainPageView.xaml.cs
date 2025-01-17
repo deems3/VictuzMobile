55d572b68c4b586a8d76413887d2b745372ec3c0
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2025-01-17 11:05:31 +0100

    Added MainPage + Community Toolkit package

--- Views/MainPageView.xaml.cs
+++ Views/MainPageView.xaml.cs
@@ -1,3 +1,5 @@
+using VictuzMobile.ViewModels;
+
 namespace VictuzMobile.Views;
 
 public partial class MainPageView : ContentPage
@@ -5,5 +7,6 @@ public partial class MainPageView : ContentPage
 	public MainPageView()
 	{
 		InitializeComponent();
-	}
+		BindingContext = new MainPageViewModel();
+    }
 }
