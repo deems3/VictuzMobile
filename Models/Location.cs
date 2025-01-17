237d42a018f34dc4333bfc4155d348f9e3e54b10
Demi <162566586+deems3@users.noreply.github.com>
2025-01-17 10:52:32 +0100

    added models

---
+++ Models/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VictuzMobile.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Street { get; set; } = null!;
+        public string Housenumber { get; set; } = null!;
+        public string PostalCode { get; set; } = null!;
+        public string Province { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Country { get; set; } = null!;
+    }
+}
