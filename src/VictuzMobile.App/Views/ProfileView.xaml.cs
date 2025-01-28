cec0f07307233f163cccde1ae7739e9a8a39c58c
MeesHvnrs <78762910+MeesHvnrs@users.noreply.github.com>
2025-01-28 11:25:45 +0100

    frontend profile view

--- src/VictuzMobile.App/Views/ProfileView.xaml.cs
+++ src/VictuzMobile.App/Views/ProfileView.xaml.cs
@@ -1,9 +1,24 @@
+using CommunityToolkit.Maui.Views;
+
 namespace VictuzMobile.App.Views;
 
 public partial class ProfileView : ContentPage
 {
-	public ProfileView()
-	{
-		InitializeComponent();
-	}
+    public ProfileView()
+    {
+        AvatarView avatarView = new()
+        {
+            Text = "PFP",
+            BackgroundColor = Colors.Black,
+            BorderColor = Color.FromArgb("#0F8390"),
+            BorderWidth = 7,
+            CornerRadius = 1000,
+            HeightRequest = 300,
+            WidthRequest = 300,
+            ImageSource = "placeholderpfp.png"
+        };
+        Content = avatarView;
+
+        InitializeComponent();
+    }
 }
