38797a0f24546ed7856bc723e1ec9fd63b06d9bd
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2025-01-29 20:53:27 +0100

    Added dark-mode and automatic theme mode. Had to make an entire new method to update the tabbar since it wouldn't auto-update.

--- src/VictuzMobile.App/Services/SecureStorageService.cs
+++ src/VictuzMobile.App/Services/SecureStorageService.cs
@@ -11,6 +11,8 @@ namespace VictuzMobile.App.Services
     {
         private const string UserIdStorageKey = "victuz_current_user_id";
         private const string UserAuthenticatedKey = "victuz_user_authenticated";
+        private const string AppDarkModeKey = "victuz_dark_mode";
+        private const string AppAutomaticThemeKey = "victuz_automatic_theme";
 
         public static void ClearCurrentUser()
         {
@@ -37,6 +39,33 @@ namespace VictuzMobile.App.Services
             var userId = await SecureStorage.GetAsync(UserIdStorageKey);
 
             return isAuthenticated is not null && userId is not null;
+        }
+
+        public static async Task<bool> GetDarkMode()
+        {
+            var darkMode = await SecureStorage.GetAsync(AppDarkModeKey);
+            return darkMode is not null && bool.Parse(darkMode);
+        }
+
+        public static async Task<bool> GetAutomaticThemeMode()
+        {
+            var automaticTheme = await SecureStorage.GetAsync(AppAutomaticThemeKey);
+            return automaticTheme is not null && bool.Parse(automaticTheme);
+        }
+
+        public static async Task SetDarkMode(bool darkMode)
+        {
+            await SecureStorage.SetAsync(AppDarkModeKey, darkMode.ToString());
+        }
+
+        public static async Task SetAutomaticThemeMode(bool automaticTheme)
+        {
+            if (automaticTheme)
+            {
+                await SetDarkMode(false);
+            }
+
+            await SecureStorage.SetAsync(AppAutomaticThemeKey, automaticTheme.ToString());
         }
     }
 }
---
+++ src/VictuzMobile.App/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using VictuzMobile.App.Services;
+
+namespace VictuzMobile.App.ViewModels
+{
+    public class SettingsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public bool IsDarkModeEnabled
+        {
+            get => _IsDarkModeEnabled;
+            set
+            {
+                _IsDarkModeEnabled = _IsAutomaticThemeEnabled ? false : value; // always disable dark mode if automatic theme is enabled otherwise set the value
+                OnPropertyChanged();
+
+                Application.Current.UserAppTheme = _IsDarkModeEnabled ? AppTheme.Dark : AppTheme.Light;
+                Task.Run(() => SecureStorageService.SetDarkMode(_IsDarkModeEnabled)); // Save the state to secure storage in a background thread so it can still be run async.
+            }
+        }
+
+        public bool IsAutomaticThemeEnabled
+        {
+            get => _IsAutomaticThemeEnabled;
+            set
+            {
+                _IsAutomaticThemeEnabled = value;
+                OnPropertyChanged();
+
+                if (_IsAutomaticThemeEnabled) // If automatic theme is enabled, disable dark mode
+                {
+                    IsDarkModeEnabled = false;
+                }
+
+                // if automatic theme is enabled, set the theme to unspecified otherwise light, since dark mode is always disabled while automatic theme is enabled
+                Application.Current.UserAppTheme = _IsAutomaticThemeEnabled ? AppTheme.Unspecified: AppTheme.Light;
+
+                Task.Run(() => SecureStorageService.SetAutomaticThemeMode(_IsAutomaticThemeEnabled));
+            }
+        }
+
+        private bool _IsDarkModeEnabled;
+        private bool _IsAutomaticThemeEnabled;
+
+        public SettingsViewModel()
+        {
+            GetStates();
+        }
+
+        private async void GetStates()
+        {
+            IsDarkModeEnabled = await SecureStorageService.GetDarkMode();
+            IsAutomaticThemeEnabled = await SecureStorageService.GetAutomaticThemeMode();
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
