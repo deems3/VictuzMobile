e8b50b201268309985f9ae52422e6d3534fd7955
Finn Curfs <2202796curfs@zuyd.nl>
2025-01-29 13:19:51 +0100

    Working qr code per userid and error if not logged in

--- src/VictuzMobile.App/Views/ProfileQRView.xaml.cs
+++ src/VictuzMobile.App/Views/ProfileQRView.xaml.cs
@@ -2,18 +2,28 @@ namespace VictuzMobile.App.Views;
 using VictuzMobile.App.Services;
 using VictuzMobile.DatabaseConfig.Models;
 
+
 public partial class ProfileQRView : ContentPage
 {
-    private readonly AuthService _authService;
+    private string QrCodeString;
     public ProfileQRView()
 	{
         InitializeComponent();
+        SetQRCode();
 	}
 
 
-
-    private void SetQRCode()
+    private async void SetQRCode()
     {
-        
+        var user = SecureStorageService.GetCurrentUserId();
+        if (user != null)
+        {
+            QrCodeString = user.Id.ToString();
+            QrCodeGenerator.Value = QrCodeString;
+        }
+        else
+        {
+            await DisplayAlert("Not Logged In", "You need to log in before viewing your QR code.", "OK");
+        }
     }
 }
