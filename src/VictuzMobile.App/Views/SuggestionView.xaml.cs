282c7bc0d4e6453765647927fc64045492eccc17
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2025-01-30 12:41:17 +0100

    Added suggestions view with basic info, not working yet.

--- src/VictuzMobile.App/Views/SuggestionView.xaml.cs
+++ src/VictuzMobile.App/Views/SuggestionView.xaml.cs
@@ -1,9 +1,43 @@
+using VictuzMobile.App.ViewModels;
+
 namespace VictuzMobile.App.Views;
 
 public partial class SuggestionView : ContentPage
 {
-	public SuggestionView(int id)
+
+	public SuggestionsViewModel ViewModel { get; set; }
+    public SuggestionView(int id, bool editmode=false)
 	{
-		InitializeComponent();
-	}
+        ViewModel = new SuggestionsViewModel(Navigation, id);
+        BindingContext = ViewModel;
+
+        InitializeComponent();
+
+        if (editmode)
+        {
+            EnableEditMode();
+        }
+    }
+
+    private void EnableEditMode()
+    {
+
+        SuggestionNameLabel.Text = "Activiteit naam";
+        SuggestionNameEntry.IsVisible = true;
+
+        SuggestionDescriptionLabel.Text = "Beschrijving";
+        SuggestionDescriptionEntry.IsVisible = true;
+
+        SuggestionOrganiserLabel.IsVisible = false;
+
+        SuggestionLocationLabel.Text = "Locatie";
+        SuggestionLocationPicker.IsVisible = true;
+        CreateNewLocationBtn.IsVisible = true;
+
+        SuggestionMaxRegistrations.Text = "Max deelnemers";
+        SuggestionMaxRegistrationsEntry.IsVisible = true;
+
+        EditSuggestionBtn.IsVisible = false;
+        SaveSuggestionBtn.IsVisible = true;
+    }
 }
