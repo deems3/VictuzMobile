2f03969fc55f242b44671d4752f18d83bb23f193
Finn Curfs <2202796curfs@zuyd.nl>
2025-01-30 14:47:07 +0100

    Working qr scanner, no database

---
+++ src/VictuzMobile.App/Views/ActivityQRScanView.xaml.cs
@@ -0,0 +1,28 @@
+namespace VictuzMobile.App.Views;
+using ZXing.Net.Maui.Controls;
+
+public partial class ActivityQRScanView : ContentPage
+{
+	public ActivityQRScanView()
+	{
+		InitializeComponent();
+        cameraBarcodeReader.Options = new ZXing.Net.Maui.BarcodeReaderOptions
+        {
+            Formats = ZXing.Net.Maui.BarcodeFormat.QrCode,
+        };
+    }
+
+    private void cameraBarcodeReader_BarcodesDetected(object sender, ZXing.Net.Maui.BarcodeDetectionEventArgs e)
+    {
+        {
+            var first = e.Results?.FirstOrDefault();
+
+            if (first is null)
+                return;
+            Dispatcher.DispatchAsync(async () =>
+            {
+                await DisplayAlert("User scanned, their ID is:", first.Value, "OK");
+            });
+        }
+    }
+}
